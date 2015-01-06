811be857f3544d58d8bf7b2b5f2a77bd7d0f01d4
Eric Barbour <emb4gu@virginia.edu>
2015-01-06 15:15:20 -0500

    Added with add and delete for posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,27 @@
 class ArticlesController < ApplicationController
-	def show
+	include ArticlesHelper
 
+	#Displays one blog post at a time
+	def show
 		@article = Article.find(params[:id])
+	end
+	#Allows the user to write a new blog post
+	def new
+		@article = Article.new
+	end
+
+	#Creates the new post
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 
 	end
 end
