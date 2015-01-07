94fffc1a48c7e552373a5bf2dc089302a17d86f5
Eric Barbour <emb4gu@virginia.edu>
2015-01-07 16:11:04 -0500

    Added image attachment capability

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107172639) do
+ActiveRecord::Schema.define(version: 20150107201643) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
